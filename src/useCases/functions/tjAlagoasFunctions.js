21fda4c91f3376e6343d194b543974c549d4504b
ManuelMolina02 <manuelanmolina@gmail.com>
2021-08-09 08:46:32 -0300

    refatorando estrutura de formulas extratores

--- src/useCases/functions/tjAlagoasFunctions.js
+++ src/useCases/functions/tjAlagoasFunctions.js
@@ -1,62 +1,124 @@
 //--------------< FUNÇÕES EXTRATORAS DE DADOS >--------------
 
-
-//Dados coletados do processo
-/*
-
-{
-  numeroProcesso: 'String',
-  situacaoProcesso: 'String',
-  classeProcesso: 'String',
-  foroProcesso: 'String'',
-  varaProcesso: 'String',
-  juizProcesso: 'String'
-}
-
-*/
-
 const extractDataHeaderTjAlagoas = () => {
-  //Selecionar dados do container principal
-  const dadosPrincipaisProcesso = document.querySelectorAll('[id="containerDadosPrincipaisProcesso"] span')
-  const maisDetalhesProcesso = document.querySelectorAll('[id="maisDetalhes"] span')
-  const maisDetalhesValores = document.querySelectorAll('[id="maisDetalhes"] div > div')
+//DADOS PRINCIPAIS
+  //Selecionando tabelas dos dados principais do processo
+  const dadosPrincipaisProcesso = document.querySelectorAll('div.row')
+  const arrayDadosPrincipais = [...dadosPrincipaisProcesso].slice(1, 4)
 
-  //Transformar objeto em array
-  const arrayDadosPrincipais = [...dadosPrincipaisProcesso, ...maisDetalhesProcesso]
+  //Selecionando dados das tabelas
+  const dataHeader = arrayDadosPrincipais.map(el => {
+    const childs = el.children
+    const arrayDataHeader = [...childs].map((el) => {
 
-  //Para cada elemento no no array retorne filtrado:
-  const filterData = arrayDadosPrincipais.filter((el) => {
-    //Elementos que NÃO INCLUAM ID com sequecia de caracteres 'label'
-    //Elementos que INCLUAM ID com sequecia de caracteres 'Processo' 
-    return !el.id.includes('label') && el.id.includes('Processo')
+      return {
+        title: el.children[0].textContent.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase(),
+        name: el.children[1].textContent.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase()
+      }
+
+    })
+    return arrayDataHeader
   })
 
-  //Para cada elemento no array retorne o innerHTML transformado:
-  const listaDadosPrincipais = filterData.map((el) =>
-    el.innerHTML
-      //Remover caracteres especiais
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      //Deixar texto em caixa alta      
-      .toLocaleUpperCase()
-  )
+//DADOS DAS PARTES
+  //Selecionando tabela das partes defensoras do processo
+  const selectPartesProcesso = document.querySelectorAll('#tableTodasPartes  tbody > tr > td')
+  const arrayPartesProcesso = [...selectPartesProcesso]
 
-  //Objeto de dados
-  const dataHeader = {
-    numeroProcesso: listaDadosPrincipais[0].replace(/\D/g, ""),
-    situacaoProcesso: listaDadosPrincipais[1],
-    classeProcesso: listaDadosPrincipais[2],
-    foroProcesso: listaDadosPrincipais[3],
-    varaProcesso: listaDadosPrincipais[4],
-    juizProcesso: listaDadosPrincipais[5],
+  //Filtrando: Nome das partes do processo { AUTORES,  REUS,  TESTEMUNHAS }
+  const partesProcesso = arrayPartesProcesso.map(el => {
+    const tipo = el.innerText.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase()
+    const nome = el.parentNode.cells[1].childNodes[0].textContent.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase()
+
+    return {
+      tipo,
+      nome,
+    }
+
+  }).filter((el, index) => index % 2 === 0)
+  //'%' retorna o valor restante da divisão os elementos
+  // restante = 0 numeros pares, igual a 1 impares
+
+  //Filtrando: Nome das defesas {ADVOGADOS, DEFENSORES PUBLICOS}
+  const defesasProcesso = arrayPartesProcesso.map(el => {
+    const dadosDefensores = el.querySelectorAll('.mensagemExibindo')
+    const arrayDadosDefensores = [...dadosDefensores].map(el => {
+      return {
+        tipo: el.innerText.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase(),
+        nome: el.nextSibling.textContent.trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleUpperCase()
+      }
+    })
+
+
+    return arrayDadosDefensores
+
+  }).filter((el, index) => index % 2 === 1)
+
+  //Filtrando: Nome das TESTEMUN
+  const testemunhas = partesProcesso.filter(el => el.tipo.includes('TESTEMUNHA'))
+
+
+//CONSTRUINDO OBJETO DE DADOS
+  const dataPartesProcess = {
+    "uf": "",
+    area: dataHeader[2][2].name,
+    juiz: dataHeader[1][4].name,
+    partes: [
+      {
+        nome: partesProcesso[0].nome,
+        tipo: partesProcesso[0].tipo,
+        advogados: defesasProcesso[0],
+      },
+      {
+        nome: partesProcesso[1].nome,
+        tipo: partesProcesso[1].tipo,
+        advogados: defesasProcesso[1],
+      },
+      {
+        testemunhas
+      }
+    ],
+    "sistema": "",
+    "segmento": "",
+    "tribunal": "",
+    "movimentos": [
+      {
+        "data": "",
+        "indice": 0,
+        "eMovimento": true,
+        "nomeOriginal": [
+          ""
+        ]
+      }
+    ],
+    "valorCausa": {
+      "moeda": "",
+      "valor": 0
+    },
+    "assuntosCNJ": [
+      {
+        "titulo": ""
+      },
+    ],
+    "urlProcesso": document.URL,
+    "grauProcesso": 0,
+    "orgaoJulgador": dataHeader[1][3].name,
+    "unidadeOrigem": dataHeader[1][2].name,
+    "classeProcessual": {
+      "nome": dataHeader[1][0].name
+    },
+    "dataDistribuicao": "",
+    "eProcessoDigital": true,
+    statusObservacao: dataHeader[0][0].name,
+    numeroProcessoUnico: dataHeader[0][0].title
   }
 
-  //Mostrar no console
-  console.log(dataHeader)
-
-  //Retornar lista fora da variavel
-  return dataHeader
+  //Retornar lista fora da variavel 
+  //ver dados da lista header inserir 'dataHeader' no return
+  return { dataPartesProcess }
 }
 
+
 module.exports = { extractDataHeaderTjAlagoas }
+
 
